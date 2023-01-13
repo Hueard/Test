2142dd2e0b4118ea88305674924b19f51a70ad41
Vladyslav <lapukhinvlad@gmail.com>
2023-01-13 21:00:49 +0100

    Second commit

---
+++ task1/Lesson1_Git_HW/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lesson1_Git_HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello Git!\n");
+        }
+    }
+}
