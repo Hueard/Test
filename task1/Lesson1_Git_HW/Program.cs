4bcffc040cbfe3ab7b1643e248a4b6ee6ce04e6c
Vladyslav <lapukhinvlad@gmail.com>
2023-01-13 21:44:31 +0100

    added for

--- task1/Lesson1_Git_HW/Program.cs
+++ task1/Lesson1_Git_HW/Program.cs
@@ -6,7 +6,13 @@ namespace Lesson1_Git_HW
     {
         static void Main(string[] args)
         {
-            Console.Write("Goodbay Git!\n");
+            Console.Write("Good Git!\n");
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
